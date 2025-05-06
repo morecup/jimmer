cd6f9f241f9cfb9c097e4f3ce595cb8a5d2809de
wcr <wcr@163.com>
2025-05-06 17:30:33 +0800

    0.2.0-0.9.81

--- project/buildSrc/src/main/kotlin/org/babyfish/jimmer/MavenPublishConfig.kt
+++ project/buildSrc/src/main/kotlin/org/babyfish/jimmer/MavenPublishConfig.kt
@@ -1,6 +1,6 @@
 package org.babyfish.jimmer
 
 object MavenPublishConfig {
-    const val VERSION = "0.1.0-0.9.81"
+    const val VERSION = "0.2.0-0.9.81"
     const val GROUP_ID = "io.github.morecup"
 }
